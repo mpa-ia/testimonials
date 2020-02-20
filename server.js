d381750514c7c44d4d96a2188e251729aaf47f92
Marysia Pabich <marysia.pabich@gmail.com>
2020-02-20 09:38:01 +0100

    Add json middleware

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 
+const db = [
+    {id: 1, author: 'John Doe', text: 'This company is worth every coin!'},
+    {id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.'},
+    {id: 3, author: 'Peter Doe', text: 'High quality products'},
+    {id: 4, author: 'Anne Doe', text: 'Perfect service'},
+    {id: 5, author: 'Simon Doe', text: 'They respect clients'},
+];
 const app = express();
+
+app.use(express.json());
 
 app.listen(8000, () => {
     console.log('Server is running on port: 8000');
