d6359938e14766f7fb826b15a461202822244e16
Marysia Pabich <marysia.pabich@gmail.com>
2020-02-20 11:40:04 +0100

    Add endpoint to get all data

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/testimonials', (req, res) => {
+    res.json(db);
+});
+
 app.listen(8000, () => {
     console.log('Server is running on port: 8000');
   });
