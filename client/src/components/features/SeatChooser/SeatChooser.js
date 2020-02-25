d9fa8a47005cb495585e5589acf1605323634ad1
Marysia Pabich <marysia.pabich@gmail.com>
2020-02-25 23:08:36 +0100

    Load seats every two minutes

--- client/src/components/features/SeatChooser/SeatChooser.js
+++ client/src/components/features/SeatChooser/SeatChooser.js
@@ -8,6 +8,7 @@ class SeatChooser extends React.Component {
   componentDidMount() {
     const { loadSeats } = this.props;
     loadSeats();
+    this.refreshSeats = setInterval(() => loadSeats(), 120000);
   }
 
   isTaken = (seatId) => {
@@ -23,6 +24,10 @@ class SeatChooser extends React.Component {
     if(seatId === chosenSeat) return <Button key={seatId} className="seats__seat" color="primary">{seatId}</Button>;
     else if(isTaken(seatId)) return <Button key={seatId} className="seats__seat" disabled color="secondary">{seatId}</Button>;
     else return <Button key={seatId} color="primary" className="seats__seat" outline onClick={(e) => updateSeat(e, seatId)}>{seatId}</Button>;
+  }
+  
+  componentWillUnmount () {
+    clearInterval(this.refreshSeats);
   }
 
   render() {
