35e44b9d884b37ff2dc4d33dc15306271b110be3
Marysia Pabich <marysia.pabich@gmail.com>
2020-02-27 13:21:17 +0100

    Add callback to throw error message in case of error

--- server.js
+++ server.js
@@ -15,10 +15,12 @@ app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
-app.use(express.static(path.join(__dirname + '/client/build')));
+app.use(express.static(path.join(__dirname + './client/build')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    res.sendFile(path.join(__dirname + './client/build/index.html'), err => {
+        if (err) res.status(500).send(err);
+    });
 });
 
 app.use((req, res) => {
