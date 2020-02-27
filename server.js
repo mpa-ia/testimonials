ed37e7f3115556289a42c829c80d8ffca246a428
Marysia Pabich <marysia.pabich@gmail.com>
2020-02-27 21:18:39 +0100

    Path correction

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static(path.join(__dirname + '/client/build')));
+
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
-app.use(express.static(path.join(__dirname + './client/build')));
-
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + './client/build/index.html'), err => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'), err => {
         if (err) res.status(500).send(err);
     });
 });
@@ -27,6 +27,6 @@ app.use((req, res) => {
     res.status(404).send({ message: 'not found...'});
 });
 
-app.listen(process.env.PORT || 8000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running on port: 8000');
-  });
+});
