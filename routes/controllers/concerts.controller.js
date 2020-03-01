cfceb8fd20f9a4c8e09c200c97b4e94cc8a48d73
Marysia Pabich <marysia.pabich@gmail.com>
2020-03-01 23:01:05 +0100

    Reorganize concerts enpoints, use mongoose

---
+++ routes/controllers/concerts.controller.js
@@ -0,0 +1,63 @@
+const Concert = require('../models/concert.model');
+
+exports.getAll = async (req, res) => {
+    try {
+        res.json(await Concert.find());
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+exports.getRandom = async (req, res) => {
+    try {
+        const count = await Concert.countDocuments();
+        const rand = Math.floor(Math.random() * count);
+        const dep = await Concert.findOne().skip(rand);
+        if (!dep) res.status(404).json({ message: 'Not found' });
+        else res.json(dep);
+    } catch (err) {
+        res.json(err);
+    }
+};
+
+exports.getById = async (req, res) => {
+    try {
+        const dep = await Concert.findById(req.params.id);
+        if (!dep) res.status(404).json({ message: 'Not found' });
+        else res.json(dep);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+exports.postNew = async (req, res) => {
+    try {
+        const { performer, genre, price, day, image } = req.body;
+        const newConcert = new Concert({ performer: performer, genre: genre, price: price, day: day, image: image });
+        await newConcert.save();
+        res.json(newConcert);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+exports.modifyById = (req, res) => {
+    const { performer, genre, price, day, image } = req.body;
+    try {
+        Concert.findByIdAndUpdate(req.params.id, { $set: { performer: performer, genre: genre, price: price, day: day, image: image }}, { new: true }, (err, doc) => {
+            err ? res.status(404).json({ message: 'Not found...' }) : res.json(doc);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
+
+exports.deleteById = (req, res) => {
+    try {
+        Concert.findByIdAndDelete(req.params.id, { new: false }, (err, doc) => {
+            err ? res.status(404).json({ message: 'Not found...' }) : res.json(doc);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+};
