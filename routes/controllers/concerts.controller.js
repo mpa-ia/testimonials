2d5401645ff54c0b8dc1ddd66e44e41e463b9828
Marysia Pabich <marysia.pabich@gmail.com>
2020-03-13 23:50:49 +0100

    Count available tickets & add it as new attribute to each concert

--- routes/controllers/concerts.controller.js
+++ routes/controllers/concerts.controller.js
@@ -1,8 +1,13 @@
 const Concert = require('../models/concert.model');
+const Seat = require('../models/seat.model');
 
 exports.getAll = async (req, res) => {
     try {
-        res.json(await Concert.find());
+        let concerts = await Concert.find();
+        let seats = await Seat.find();
+        const freeTickets = 50 - seats.filter(seat => concerts.map(concert => concert._doc.day).includes(seat.day)).length;
+        concerts = concerts.map(concert => ({ ...concert._doc, tickets: freeTickets }));
+        res.json(concerts);
     } catch (err) {
         res.status(500).json(err);
     }
@@ -44,7 +49,7 @@ exports.postNew = async (req, res) => {
 exports.modifyById = (req, res) => {
     const { performer, genre, price, day, image } = req.body;
     try {
-        Concert.findByIdAndUpdate(req.params.id, { $set: { performer: performer, genre: genre, price: price, day: day, image: image }}, { new: true }, (err, doc) => {
+        Concert.findByIdAndUpdate(req.params.id, { $set: { performer: performer, genre: genre, price: price, day: day, image: image } }, { new: true }, (err, doc) => {
             err ? res.status(404).json({ message: 'Not found...' }) : res.json(doc);
         });
     } catch (err) {
