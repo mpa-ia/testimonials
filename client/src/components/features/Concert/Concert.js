8bb8ae63ea9e1f1a33329b47b7d990e78693eca3
Marysia Pabich <marysia.pabich@gmail.com>
2020-03-13 23:51:38 +0100

    Display available tickets on main page

--- client/src/components/features/Concert/Concert.js
+++ client/src/components/features/Concert/Concert.js
@@ -3,20 +3,21 @@ import { Row, Col } from 'reactstrap';
 
 import './Concert.scss';
 
-const Concert = ({ performer, price, genre, day, image }) => (
+const Concert = ({ performer, price, genre, day, image, tickets }) => (
   <article className="concert">
     <Row noGutters>
       <Col xs="6">
         <div className="concert__image-container">
-          <img className="concert__image-container__img" src={image} alt={performer}/>
+          <img className="concert__image-container__img" src={image} alt={performer} />
         </div>
       </Col>
       <Col xs="6">
         <div className="concert__info">
-          <img className="concert__info__back" src={image} alt={performer}/>
-          <h2 className="concert__info__performer">{ performer }</h2>
-          <h3 className="concert__info__genre">{ genre }</h3>
-          <p className="concert__info__day-n-price">Day: {day}, Price: { price }$</p>
+          <img className="concert__info__back" src={image} alt={performer} />
+          <h2 className="concert__info__performer">{performer}</h2>
+          <h3 className="concert__info__genre">{genre}</h3>
+          <small className="concert__info__tickets">Only {tickets} tickets left!</small>
+          <p className="concert__info__day-n-price">Day: {day}, Price: {price}$</p>
         </div>
       </Col>
     </Row>
